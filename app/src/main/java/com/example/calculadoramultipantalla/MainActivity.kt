290d988dab639440dde5d4dca9b0617d097c5180
franklin_cuascota <franklin.cuascota@ister.edu.ec>
2026-05-26 17:24:52 -0500

    pantalla 1 y ui

--- app/src/main/java/com/example/calculadoramultipantalla/MainActivity.kt
+++ app/src/main/java/com/example/calculadoramultipantalla/MainActivity.kt
@@ -19,9 +19,9 @@ import androidx.navigation.navArgument
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity() { //Cambié AppCompatActivity por ComponentActivity
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState) // Cambié setContentView por setContent
         setContent {
             MaterialTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
@@ -32,6 +32,34 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun InputScreen(navController: NavController) { // Cambié el nombre de la función a InputScreen y agregué NavController como parámetro
+    // Variables de estado para los campos de entrada
+    var nombre by remember { mutableStateOf("") }
+    var peso by remember { mutableStateOf("") }
+    var altura by remember { mutableStateOf("") }
+
+// Interfaz de usuario para ingresar datos
+    Column(modifier = Modifier.padding(16.dp).fillMaxSize()) {
+        OutlinedTextField(
+            value = nombre, onValueChange = { nombre = it },
+            label = { Text("Nombre") }, modifier = Modifier.fillMaxWidth() // Agregué un campo de texto para el nombre
+        )
+        Spacer(modifier = Modifier.height(8.dp)) // Agregué un espacio entre los campos de texto
+        OutlinedTextField(
+            value = peso, onValueChange = { peso = it },
+            label = { Text("Peso (kg)") }, modifier = Modifier.fillMaxWidth() // Agregué un campo de texto para el peso
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        OutlinedTextField(
+            value = altura, onValueChange = { altura = it },
+            label = { Text("Altura (m)") }, modifier = Modifier.fillMaxWidth()  // Agregué un campo de texto para la altura
+        )
+        Spacer(modifier = Modifier.height(16.dp)) // Agregué un espacio antes del botón
+
+        Button(onClick = { }, modifier = Modifier.fillMaxWidth()) { Text("Calcular") }  //Boton para calcular el IMC, aún sin funcionalidad
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
